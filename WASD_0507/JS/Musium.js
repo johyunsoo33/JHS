e0d7f2d861c39c43fc7cef42bd4dc084fc3dd084
J.H.S_0616 <johyunsoo369@naver.com>
2024-10-21 22:20:05 +0900

    Delete WASD_0507 directory

--- WASD_0507/JS/Musium.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween:20,
-    loop: true
-  });
-
-document.getElementById("myButton").onclick = function() {
-    location.href = "Musium_Review.html";
-};
